cf7b6e2d01eade4dcf66dda6daae8c9cbbcf760c
Matt <matt@mralston.co.uk>
2020-08-27 22:46:03 +0100

    Update Calendar.php

--- src/MaddHatter/LaravelFullcalendar/Calendar.php
+++ src/MaddHatter/LaravelFullcalendar/Calendar.php
@@ -3,6 +3,7 @@
 use ArrayAccess;
 use DateTime;
 use Illuminate\View\Factory;
+use Illuminate\Support\Str;
 
 class Calendar
 {
@@ -126,7 +127,7 @@ class Calendar
             return $this->id;
         }
 
-        $this->id = str_random(8);
+        $this->id = Str::random(8);
 
         return $this->id;
     }
