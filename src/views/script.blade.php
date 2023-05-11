48e32db87e5350dd97c70df3c653de5b8f90209d
Matt <matt@mralston.co.uk>
2023-05-11 11:15:59 +0100

    Update script.blade.php
    Allow for jQuery using <script defer>.

--- src/views/script.blade.php
+++ src/views/script.blade.php
@@ -1,5 +1,9 @@
 <script>
-    $(document).ready(function(){
-        $('#calendar-{{ $id }}').fullCalendar({!! $options !!});
+    // Wait for DOMContentLoaded event to emulate <script defer>
+    // to ensure jQuery has loaded if it also has a defer attribute
+    window.addEventListener('DOMContentLoaded', function() {
+        $(document).ready(function(){
+            $('#calendar-{{ $id }}').fullCalendar({!! $options !!});
+        });
     });
 </script>
